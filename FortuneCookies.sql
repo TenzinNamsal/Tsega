18588e31e57964d97aabaa05f22cd00d72b43a97
TenzinNamsal <125288509+TenzinNamsal@users.noreply.github.com>
2023-04-07 13:33:09 -0500

    Add files via upload
    better luck

---
+++ FortuneCookies.sql
@@ -0,0 +1,23 @@
+create database FortuneCookies;
+use FortuneCookies;
+Create Table FortuneCookies
+(cookiesid int not null,
+cookiesMessage varchar(50));
+select * from FortuneCookies;
+insert into FortuneCookies(cookiesid,cookiesMessage)
+value(1, 'you are strong');
+
+insert into FortuneCookies(cookiesid,cookiesMessage)
+value(2, 'you are intelligent');
+
+insert into FortuneCookies(cookiesid,CookiesMessage)
+value(3,'you need to workhard');
+
+insert into FortuneCookies(cookiesid, CookiesMessage)
+Value(4,'you need to be smart');
+ select * from FortuneCookies;
+insert into FortuneCookies(cookiesid, CookiesMessage)
+Value(5,'you need to puntual');
+insert into FortuneCookies(cookiesid, CookiesMessage)
+Value(6,'you will be Software Engineer ');
+select*from FortuneCookies;
